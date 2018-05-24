d56c11686dbd0a9feb7754b3808ff51096a7ba78
chenl4 <chenl4@rose-hulman.edu>
2018-05-24 12:05:50 -0400

    blah

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS in PRINTING-TO-CONSOLE problems.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lilin Chen.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -87,6 +87,20 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # ------------------------------------------------------------------
+
+    ori_seq = ''
+    seq = ''
+
+    for k in range(r, 0, -1):
+        for j in range(r, 0, -1):
+            if j <= k:
+                seq = seq + '+'
+            if j > k:
+                seq = seq + ' '
+        seq = seq + '!'
+        for i in range(k, 0, -1):
+            seq = seq + i
+
 
 
 # ----------------------------------------------------------------------
